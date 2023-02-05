4a1b4423fe20032ccea0a3d2c1d799178089e9de
Douglas Kosvoski <douglas.contactpro@gmail.com>
2023-02-05 15:05:16 -0300

    Glow effect

--- Three.js/CG_Project/app.js
+++ Three.js/CG_Project/app.js
@@ -7,33 +7,66 @@ var player, control;
 // how much the characters turn
 var steering_bias = 1.6;
 // base speed
-var steering_speed = 200;
+var steering_speed = 150;
 var outline_effect = true;
 
 var player_radius = 15;
 var player_segments = player_radius * 1.5;
-var player_geometry = new THREE.SphereGeometry(player_radius, player_segments, player_segments);
+var player_geometry = new THREE.SphereGeometry(
+  player_radius,
+  player_segments,
+  player_segments
+);
 var player_material = new THREE.MeshNormalMaterial();
 // var player_material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
 
-var show_minimap = true;
-var minimap, minimap_width = 500, minimap_height = 500, minimap_composer;
+var show_minimap = false;
+var minimap,
+  minimap_width = 500,
+  minimap_height = 500,
+  minimap_composer;
 
 this.init();
 this.animate();
 
 function init() {
   scene = new THREE.Scene();
+
   add_camera();
   add_renderer();
   add_events();
   add_controls();
   add_lights();
   add_plane_track();
-  add_skybox();
+  // add_skybox();
   add_stats();
   add_axis();
   add_player();
+
+  var glow_geometry = new THREE.SphereGeometry(
+    player_radius - 1,
+    player_segments - 1,
+    player_segments - 1
+  );
+  var glow_material = new THREE.MeshLambertMaterial({ color: 0x000088 });
+  glow_mesh = new THREE.Mesh(glow_geometry, glow_material);
+  glow_mesh.position = player.position
+
+  scene.add(glow_mesh);
+
+  // SUPER SIMPLE GLOW EFFECT
+  // use sprite because it appears the same from all angles
+  var glowSpriteMaterial = new THREE.SpriteMaterial({
+    map: new THREE.ImageUtils.loadTexture("./glow.png"),
+    useScreenCoordinates: false,
+    alignment: THREE.SpriteAlignment.center,
+    color: 0x0000ff,
+    transparent: false,
+    blending: THREE.AdditiveBlending,
+  });
+  var glow_sprite = new THREE.Sprite(glowSpriteMaterial);
+  glow_sprite.scale.set(80, 80, 1.0);
+  glow_mesh.add(glow_sprite); // this centers the glow at the mesh
 
   if (show_minimap) {
     add_minimap();
@@ -42,8 +75,6 @@ function init() {
   var ambientLight = new THREE.AmbientLight(0x111111);
   scene.add(ambientLight);
   control = new THREE.OrbitControls(camera, scene);
-
-
 }
 
 function add_player() {
@@ -57,10 +88,13 @@ function add_player() {
 }
 
 function apply_player_outline_effect() {
-  var outline_color = 'red'
+  var outline_color = "red";
   var thickness = 1.05;
 
-  var material = new THREE.MeshBasicMaterial({ color: outline_color, side: THREE.BackSide });
+  var material = new THREE.MeshBasicMaterial({
+    color: outline_color,
+    side: THREE.BackSide,
+  });
   var outline = new THREE.Mesh(player_geometry, material);
   outline.position = player.position;
   outline.scale.multiplyScalar(thickness);
@@ -95,13 +129,14 @@ function add_renderer() {
 
 function add_camera() {
   // original fov = 45
-  var w = window.innerWidth, h = window.innerHeight;
+  var w = window.innerWidth,
+    h = window.innerHeight;
 
   var VIEW_ANGLE = 45;
   var ASPECT = w / h;
   var NEAR = 0.1;
-  var FAR = 20000;
-  
+  var FAR = 10000;
+
   camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
   camera.position.set(100, 100, 100);
   camera.lookAt(scene.position);
@@ -110,17 +145,18 @@ function add_camera() {
 
 function add_minimap() {
   minimap = new THREE.OrthographicCamera(
-    -1400,		// Left
-    1400,		// Right
-    1400,		// Top
-    -1400,		// Bottom
-    1,            			// Near 
-    1000);           			// Far 
+    -1500, // Left
+    1500, // Right
+    1500, // Top
+    -1500, // Bottom
+    1, // Near
+    1000
+  ); // Far
   minimap.up = new THREE.Vector3(0, 0, -1);
   minimap.lookAt(new THREE.Vector3(0, -1, 0));
   minimap.position.y = 100;
-  minimap.rotation.z = Math.PI / 2;
-  minimap.position.z = -1200;
+  // minimap.rotation.z = Math.PI / 2;
+  minimap.position.z = -500;
 
   scene.add(minimap);
 
@@ -138,7 +174,7 @@ function add_minimap() {
   var effectFXAA = new THREE.ShaderPass(THREE.FXAAShader);
   var width = window.innerWidth || 2;
   var height = window.innerHeight || 2;
-  effectFXAA.uniforms['resolution'].value.set(1 / width, 1 / height);
+  effectFXAA.uniforms["resolution"].value.set(1 / width, 1 / height);
 
   // var effectCopy = new THREE.ShaderPass( THREE.CopyShader );
   effectFXAA.renderToScreen = true;
@@ -148,14 +184,22 @@ function add_minimap() {
   // composer.addPass( effectCopy );
 
   // composer for minimap
-  var parameters = { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBFormat, stencilBuffer: false };
-  minimap_composer = new THREE.EffectComposer(renderer, new THREE.WebGLRenderTarget(512, 512));
+  var parameters = {
+    minFilter: THREE.LinearFilter,
+    magFilter: THREE.LinearFilter,
+    format: THREE.RGBFormat,
+    stencilBuffer: false,
+  };
+  minimap_composer = new THREE.EffectComposer(
+    renderer,
+    new THREE.WebGLRenderTarget(512, 512)
+  );
   minimap_composer.setSize(512, 512);
   var renderModel2 = new THREE.RenderPass(scene, minimap);
   minimap_composer.addPass(renderModel2);
   var effectFXAA2 = new THREE.ShaderPass(THREE.FXAAShader);
-  effectFXAA2.uniforms['resolution'].value.set(1 / 512, 1 / 512);
-  // effectFXAA2.renderToScreen = true;	
+  effectFXAA2.uniforms["resolution"].value.set(1 / 512, 1 / 512);
+  // effectFXAA2.renderToScreen = true;
   minimap_composer.addPass(effectFXAA2);
   var effectCopy2 = new THREE.ShaderPass(THREE.CopyShader);
   effectCopy2.renderToScreen = true;
@@ -232,10 +276,10 @@ function update() {
   keyboard.update();
 
   var delta = clock.getDelta();
+  // player.translateY(-0.1);
   var moveDistance = steering_speed * delta;
   var rotateAngle = (Math.PI / 2) * delta + steering_bias / 100;
   var rotation_matrix = new THREE.Matrix4().identity();
-
 
   if (keyboard.pressed("up") || keyboard.pressed("W")) {
     player.translateZ(-moveDistance);
@@ -245,8 +289,7 @@ function update() {
     if (keyboard.pressed("right") || keyboard.pressed("D")) {
       player.rotateOnAxis(new THREE.Vector3(0, 1, 0), -rotateAngle);
     }
-  }
-  else if (keyboard.pressed("down") || keyboard.pressed("S")) {
+  } else if (keyboard.pressed("down") || keyboard.pressed("S")) {
     player.translateZ(moveDistance);
     if (keyboard.pressed("left") || keyboard.pressed("A")) {
       player.rotateOnAxis(new THREE.Vector3(0, 1, 0), -rotateAngle);
@@ -271,11 +314,14 @@ function update() {
       )
     );
 
+  glow_mesh.position = player.position;
+
   stats.update();
 }
 
 function render() {
-  var w = window.innerWidth, h = window.innerHeight;
+  var w = window.innerWidth,
+    h = window.innerHeight;
 
   // setViewport parameters:
   //  lower_left_x, lower_left_y, viewport_width, viewport_height
@@ -289,8 +335,13 @@ function render() {
   }
   renderer.clear(false, true, false);
 
-  renderer.setViewport(5, h - minimap_height - 5, minimap_width, minimap_height);
-  
+  renderer.setViewport(
+    5,
+    h - minimap_height - 5,
+    minimap_width,
+    minimap_height
+  );
+
   if (show_minimap) {
     renderer.render(scene, minimap);
   }
